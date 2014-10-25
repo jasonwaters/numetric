c5d098206d177000e2e1393451fcf08c0f553131
Jason Waters <jason@myheck.net>
2014-10-24 21:51:52 -0600

    now using routes to load templates based on url.  A dummy maps view is stubbed in.

--- js/app.js
+++ js/app.js
@@ -27,217 +27,31 @@
 	];
 
 
-	var app = angular.module('numetric', ['numetric.services', 'numetric.filters', 'numetric.directives', 'google-maps'.ns()]);
+	var app = angular.module('numetric', ['numetric.services', 'numetric.filters', 'numetric.directives', 'numetric.controllers', 'google-maps'.ns(), 'ngRoute']);
 
-	app.config(['GoogleMapApiProvider'.ns(), function (GoogleMapApi) {
+	app.config(['GoogleMapApiProvider'.ns(), '$routeProvider', function (GoogleMapApi, $routeProvider) {
 		GoogleMapApi.configure({
 			key: 'AIzaSyALf5-sdlvQTvrN827e_FPKrV66JkKV5tY',
 			v: '3.17',
 			libraries: 'weather,geometry,visualization'
 		});
+
+
+		$routeProvider.
+			when('/dashboard', {
+				templateUrl: 'partials/dashboard-view.html',
+				controller: 'numetric-main',
+			}).
+			when('/map', {
+				templateUrl: 'partials/map-view.html',
+				controller: 'numetric-map',
+			}).
+			otherwise({
+				redirectTo: '/dashboard'
+			});
+
 	}]);
 
-	app.controller('numetric-main', ['$scope', 'GoogleMapApi'.ns(), 'pavement-service', function ($scope, GoogleMapApi, pavementService) {
 
-		var setSelected = function(model) {
-			_.forEach($scope.polygons, function(polygon) {
-				polygon.fill.color = NM.FILL_COLOR_DEFAULT;
-				polygon.fill.opacity = NM.FILL_OPACITY_DEFAULT;
-				polygon.stroke.color = NM.STROKE_COLOR_DEFAULT;
-				polygon.stroke.weight = NM.STROKE_WEIGHT_DEFAULT;
-			});
-
-			model.fill.color = NM.FILL_COLOR_SELECTED;
-			model.fill.opacity = NM.FILL_OPACITY_SELECTED;
-			model.stroke.color = NM.STROKE_COLOR_SELECTED;
-			model.stroke.weight = NM.STROKE_WEIGHT_SELECTED;
-
-			$scope.selected = _.find($scope.polygons, function(polygon) {
-				return polygon.isSelected();
-			});
-
-			$scope.treatments = pavementService.getTreatments($scope.selected.id);
-			$scope.averageCondition = pavementService.getAverageConditionHistory($scope.selected.id);
-			$scope.AADT = pavementService.getAADT($scope.selected.id);
-
-			renderAverageConditionLineChart();
-			renderAverageConditionBarChart();
-			renderAADTLineChart();
-		};
-
-		$scope.polygonEvents = {
-			'click': function(polygon, eventName, model, args) {
-				setSelected(model);
-			}
-		};
-
-		$scope.map = {
-			center: {
-				latitude: 40.6357,
-				longitude: -111.9047
-			},
-			zoom: 16,
-			options: {
-				panControl: false,
-				streetViewControl: false,
-				zoomControl: false
-				//styles: mapStyle
-			}
-		};
-
-
-		GoogleMapApi.then(function(maps) {
-			$scope.map.options.mapTypeId = maps.MapTypeId.HYBRID;
-			//$scope.map.options.mapTypeId = maps.MapTypeId.ROADMAP;
-		});
-
-
-		$scope.selected = null;
-		$scope.charts = {};
-		$scope.treatments = [];
-		$scope.polygons = pavementService.getSegments();
-		setSelected($scope.polygons[0]);
-
-		function renderAverageConditionLineChart() {
-
-			var treatmentLines = [];
-
-			_.forEach($scope.treatments, function(treatment) {
-				treatmentLines.push({
-					'value': moment(treatment.dateCompleted).startOf('year').format('YYYY-MM-DD'),
-					'text': treatment.id
-				});
-			});
-
-			$scope.charts.averageConditionLine = c3.generate({
-				bindto: '#chart-average-condition-line',
-				size: {
-					height: 230
-				},
-				data: {
-					x: 'x',
-					columns: [
-						['x'].concat($scope.averageCondition.keys),
-						['Condition'].concat($scope.averageCondition.values)
-					],
-					type: 'area'
-				},
-				color: {
-					pattern: ['#1daed7']
-				},
-				axis: {
-					x: {
-						type: 'timeseries',
-						tick: {
-							format: '%Y'
-						}
-					}
-				},
-				legend: {
-					show: false
-				},
-				grid: {
-					x: {
-						lines: treatmentLines
-					}
-				}
-			});
-		}
-
-		function renderAverageConditionBarChart() {
-			var treatmentLines = [];
-
-			_.forEach($scope.treatments, function(treatment) {
-				treatmentLines.push({
-					'value': moment(treatment.dateCompleted).startOf('year').format('YYYY-MM-DD'),
-					'text': treatment.id
-				});
-			});
-
-			$scope.charts.averageConditionBar = c3.generate({
-				bindto: '#chart-average-condition-bar',
-				size: {
-					height: 230
-				},
-				data: {
-					x : 'x',
-					columns: [
-						['x'].concat($scope.averageCondition.keys),
-						['Condition'].concat($scope.averageCondition.values)
-					],
-					type: 'bar'
-				},
-				groups: [
-					['Condition']
-				],
-				color: {
-					pattern: ['#43c1df', '#8e837e']
-				},
-				bar: {
-					width: {
-						ratio: 0.5 // this makes bar width 50% of length between ticks
-					}
-				},
-				axis: {
-					x: {
-						type: 'timeseries',
-						tick: {
-							format: '%Y'
-						}
-					}
-				},
-				grid: {
-					focus: {
-						show: false
-					},
-					x: {
-						lines: treatmentLines
-					}
-				},
-				legend: {
-					show: false
-				}
-			});
-		}
-
-		function renderAADTLineChart() {
-			$scope.charts.AADT = c3.generate({
-				bindto: '#chart-AADT-line',
-				size: {
-					height: 230
-				},
-				data: {
-					x: 'x',
-					columns: [
-						['x'].concat($scope.AADT.keys),
-						['Traffic'].concat($scope.AADT.values)
-					],
-					type: 'area'
-				},
-				color: {
-					pattern: ['#1daed7']
-				},
-
-				axis: {
-					x: {
-						type: 'timeseries',
-						tick: {
-							format: '%Y'
-						}
-					},
-					y: {
-						tick: {
-							format: function(y) {
-								return numeral(y).format('0a');
-							}
-						}
-					}
-				},
-				legend: {
-					show: false
-				}
-			});
-		}
-	}]);
 })();
 
--- js/directives.js
+++ js/directives.js
@@ -18,4 +18,40 @@
 		};
 	});
 
+
+	//http://stackoverflow.com/questions/12295983/set-active-tab-style-with-angularjs
+	app.directive('detectActiveTab', ['$location',
+		function($location) {
+			return {
+				link: function postLink(scope, element, attrs) {
+					scope.$on("$routeChangeSuccess", function(event, current, previous) {
+						/*
+						 Designed for full re-usability at any path, any level, by using data from attrs.
+						 Declare like this: <li class="nav_tab"><a href="#/home" active-tab="1">HOME</a></li>
+						 */
+
+						// This var grabs the tab-level off the attribute, or defaults to 1
+						var pathLevel = attrs.detectActiveTab || 1,
+						// This var finds what the path is at the level specified
+							pathToCheck = $location.path().split('/')[pathLevel] || "current $location.path doesn't reach this level",
+						// This var finds grabs the same level of the href attribute
+							tabLink = attrs.href.split('/')[pathLevel] || "href doesn't include this level";
+						//console.log(pathToCheck);
+						//console.log(tabLink);
+						// Above, we use the logical 'or' operator to provide a default value in cases
+						// where 'undefined' would otherwise be returned.
+						// This prevents cases where undefined===undefined, possibly causing multiple tabs to be 'active'.
+
+						// now compare the two:
+						var li = $(element).parent('li');
+						if (pathToCheck === tabLink) {
+							li.addClass("active");
+						} else {
+							li.removeClass("active");
+						}
+					});
+				}
+			};
+		}
+	]);
 })();
